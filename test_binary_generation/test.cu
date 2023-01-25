9b67caf97141c789b52fc72494241d7e95eef9c8
Graham Markall <gmarkall@nvidia.com>
2023-01-25 11:33:58 +0000

    Add some test binary generation infrastructure

---
+++ test_binary_generation/test.cu
@@ -0,0 +1,13 @@
+#include <cuda_fp16.h>
+
+__device__
+__half test_add_fp16(__half arg1, __half arg2)
+{
+  return __hadd(arg1, arg2);
+}
+
+__device__
+bool test_cmp_fp16(__half arg1, __half arg2)
+{
+  return __heq(arg1, arg2);
+}
