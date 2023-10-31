bc7116fd0e46c3aa0270eacc82a8b40943fa3b90
brandon-b-miller <brmiller@nvidia.com>
2023-10-31 09:30:18 -0700

    install ctk

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2023, NVIDIA CORPORATION
+
+# install CUDAToolKit version 12.2
+wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda_12.2.0_535.54.03_linux.run
+sh cuda_12.2.0_535.54.03_linux.run --silent --toolkit --override
