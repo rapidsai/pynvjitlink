351fd7e0817362a74ca290dc57b00fb0750c67fa
brandon-b-miller <brmiller@nvidia.com>
2023-10-31 09:34:52 -0700

    add -q for now

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -2,5 +2,5 @@
 # Copyright (c) 2023, NVIDIA CORPORATION
 
 # install CUDAToolKit version 12.2
-wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda_12.2.0_535.54.03_linux.run
+wget -q https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda_12.2.0_535.54.03_linux.run
 sh cuda_12.2.0_535.54.03_linux.run --silent --toolkit --override
