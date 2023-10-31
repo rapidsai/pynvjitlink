c3ebe4cff69d4b5ea0bfffe2d1f5c90de9a70a37
brandon-b-miller <brmiller@nvidia.com>
2023-10-31 09:46:17 -0700

    minor fixes

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -2,5 +2,5 @@
 # Copyright (c) 2023, NVIDIA CORPORATION
 
 # install CUDAToolKit version 12.2
-sudo apt-get update
-sudo apt-get install nvidia-cuda-toolkit-12.2
+apt-get update
+apt-get install nvidia-cuda-toolkit-12.2
