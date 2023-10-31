e58540ab6b79f244f7ef04213bffdb1937d45c8b
brandon-b-miller <brmiller@nvidia.com>
2023-10-31 12:21:38 -0700

    add tests and logging

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -5,6 +5,18 @@
 apt-get update
 apt-get -y install cuda-toolkit-12-2
 
+rapids-logger "Check conda environment"
+
 conda list
+
+rapids-logger "Building wheel"
+
 pip wheel .
-ls
+
+rapids-logger "Building tests"
+
+cd test_binary_generation && make
+
+rapids-logger "Running Tests"
+
+cd .. && pytest -v
