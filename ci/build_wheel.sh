37ce73391a42cb2b8eecac7fe3ce70bcf32cce6e
brandon-b-miller <brmiller@nvidia.com>
2023-11-02 09:05:08 -0700

    fix centos cuda install

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -5,6 +5,8 @@ set -e
 
 rapids-logger "Install CUDA Toolkit"
 yum update
+yum install -y epel-release
+yum install -y nvidia-driver-latest-dkms
 yum install -y cuda-toolkit-12-3
 
 rapids-logger "Check conda environment"
