abeae673d359a8704b4aa7567f2a7282b9541125
brandon-b-miller <brmiller@nvidia.com>
2023-11-03 09:05:07 -0700

    remove testing phase for now, will test on downstream jobs instead later

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -16,17 +16,3 @@ conda list
 rapids-logger "Build wheel"
 
 pip wheel .
-
-rapids-logger "Install wheel"
-
-pip install pynvjitlink-0.1.0-cp310-cp310-manylinux_2_35_x86_64.whl
-
-rapids-logger "Build tests"
-
-cd test_binary_generation && make
-
-rapids-logger "Run tests"
-
-cd ..
-conda install -y pytest
-py.test pynvjitlink/tests
