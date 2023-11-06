1b2ce4d60b3821a0a44f4f38ee9b3e79d8717621
brandon-b-miller <brmiller@nvidia.com>
2023-11-06 11:25:51 -0800

    expand test_wheel.sh

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 # Copyright (c) 2023, NVIDIA CORPORATION
+
+set -e
+
+rapids-logger "Install CUDA Toolkit"
+yum update
+yum install -y epel-release
+yum install -y nvidia-driver-latest-dkms
+yum install -y cuda-toolkit-12-3
+
+
+rapids-logger "Install Python"
+
+python_version="${1:-3.10}"
+conda install -y python="$python_version"
+
+rapids-logger "Check conda environment"
+
+conda list
+
+rapids-logger "Install wheel"
+
+pip install ./wheel-build-${python_version}/*.whl
+
+rapids-logger "Build Tests"
+cd test_binary_generation && make
+
+rapids-logger "Run Tests"
+conda install -y pytest
+py.test tests
