f0168059ba5ef680f103634c2913f9ec66cfa66d
brandon-b-miller <brmiller@nvidia.com>
2023-11-15 18:03:42 -0800

    address reviews

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -8,12 +8,6 @@ yum install -y epel-release
 yum install -y nvidia-driver-latest-dkms
 yum install -y cuda-toolkit-12-3
 
-rapids-logger "Install GCC"
-yum install -y centos-release-scl
-yum install -y devtoolset-9
-
-source scl_source enable devtoolset-9
-
 rapids-logger "Check conda environment"
 conda list
 
