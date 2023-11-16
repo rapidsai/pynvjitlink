7e48e2baa1d5df54c478b8164ed484bf79eff8b0
brandon-b-miller <brmiller@nvidia.com>
2023-11-16 06:57:30 -0800

    Revert "try removing cache again"
    This reverts commit 78b0b4119d7b04c5fb945b620ae323c990a551af.

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -12,5 +12,6 @@ rapids-logger "Check conda environment"
 conda list
 
 rapids-logger "Build wheel"
+export SCCACHE_S3_NO_CREDENTIALS=1
 mkdir -p ./wheel-build
 pip wheel . --wheel-dir=./wheel-build -vvv
