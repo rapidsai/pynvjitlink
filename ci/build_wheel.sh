27a5e70c96f3938e7052ad2724e027d99feee27f
brandon-b-miller <brmiller@nvidia.com>
2023-11-28 19:59:57 -0800

    trim path

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -13,10 +13,10 @@ conda list
 
 rapids-logger "Build wheel"
 mkdir -p ./wheel-build
-pip wheel . --wheel-dir=./wheel-build -vv
+pip wheel . --wheel-dir=./wheel-build -vvv
 
-WHEEL_NAME=$(find "./wheel-build" -type f -name "*.whl")
-echo "WHEEL_NAME=${WHEEL_NAME}"
+WHEEL_PATH=$(find "./wheel-build" -type f -name "*.whl")
+echo "WHEEL_PATH=${WHEEL_PATH}"
 
 rapids-logger "Upload Wheel"
-RAPIDS_BUILD_TYPE="branch" rapids-upload-to-s3 pynvjitlink-cu12 ./wheel-build/${WHEEL_NAME}
+RAPIDS_BUILD_TYPE="branch" rapids-upload-to-s3 pynvjitlink-cu12 ${WHEEL_PATH}
