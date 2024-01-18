90bc3f2686691f5c91e6346b6ab0ceb221f8ce1a
Graham Markall <535640+gmarkall@users.noreply.github.com>
2024-01-18 20:34:45 +0000

    Fix #39 by matching current Numba implementation of `add_cu()` (#40)
    Numba 0.58 implements `add_cu()` differently to Numba 0.57 and below.
    These changes bring the implementation in the patch in line with Numba
    0.58 onwards, and makes the minimum supported Numba version 0.58, so
    that we don't have to support multiple different implementations. There
    is presently no widespread production use (or perhaps no production use
    at all) of pynvjitlink with Numba 0.57, so keeping Numba 0.57
    compatibility is not considered.
    
    Testing of the patch against the current Numba version is added to the
    CI configuration.
    
    The version number is bumped to 0.1.12 to reflect the nature of this
    change.
    
    Fixes #39

--- ci/test_patch.sh
+++ ci/test_patch.sh
@@ -8,13 +8,10 @@ set -euo pipefail
 rapids-logger "Generate testing dependencies"
 # TODO: Replace with rapids-dependency-file-generator
 rapids-mamba-retry create -n test \
-    c-compiler \
-    cxx-compiler \
-    cuda-nvcc \
+    cuda-nvcc-impl \
+    cuda-nvrtc \
     cuda-version=${RAPIDS_CUDA_VERSION%.*} \
     "numba>=0.58" \
-    make \
-    pytest \
     python=${RAPIDS_PY_VERSION}
 
 # Temporarily allow unbound variables for conda activation.
@@ -23,19 +20,12 @@ conda activate test
 set -u
 
 PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
-RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}/
-mkdir -p "${RAPIDS_TESTS_DIR}"
 
 rapids-print-env
 
 rapids-mamba-retry install \
   --channel "${PYTHON_CHANNEL}" \
   pynvjitlink
-
-rapids-logger "Build Tests"
-pushd test_binary_generation
-make
-popd
 
 rapids-logger "Check GPU usage"
 nvidia-smi
@@ -44,11 +34,8 @@ EXITCODE=0
 trap "EXITCODE=1" ERR
 set +e
 
-rapids-logger "pytest pynvjitlink"
-pytest \
-  --cache-clear \
-  --junitxml="${RAPIDS_TESTS_DIR}/junit-pynvjitlink.xml" \
-  -v
+rapids-logger "Test Numba with patch"
+python ci/run_patched_numba_tests.py numba.cuda.tests -v -m
 
 rapids-logger "Test script exiting with value: $EXITCODE"
 exit ${EXITCODE}
