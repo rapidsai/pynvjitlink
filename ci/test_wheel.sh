fe47f82accaf7f2f73daeb6d6aaec1104849437b
jakirkham <jakirkham@gmail.com>
2024-02-14 18:13:32 -0800

    `pip install` dependencies first
    Also make sure to include `numba` and anything else done later like
    `pytest`.

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -3,6 +3,12 @@
 
 set -euo pipefail
 
+rapids-logger "Install testing dependencies"
+# TODO: Replace with rapids-dependency-file-generator
+python -m pip install \
+    "numba>=0.58" \
+    pytest
+
 rapids-logger "Download Wheel"
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="pynvjitlink_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist/
@@ -20,5 +26,4 @@ make
 popd
 
 rapids-logger "Run Tests"
-python -m pip install pytest
 python -m pytest pynvjitlink/tests
