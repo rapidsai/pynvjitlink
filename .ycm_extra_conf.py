38d1d5670ed153a80186d8a0391ddadc134df3d5
jakirkham <jakirkham@gmail.com>
2024-03-06 14:25:52 -0800

    Rewrite YCM conf with `sysconfig`

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,24 +1,18 @@
-import os
-import sys
-
+import sysconfig
 from pathlib import Path
 
-CONDA_PREFIX = os.environ["CONDA_PREFIX"]
+PREFIX = sysconfig.get_config_var("prefix")
 NVJITLINK_DIR = Path(__file__).parent
-PYTHON_VERSION = sys.version_info
-PYTHON_INCLUDE_NAME = "python%d.%d" % PYTHON_VERSION
 
-CONDA_INCLUDE_DIR = Path(CONDA_PREFIX, "include")
-PYTHON_INCLUDE_DIR = Path(CONDA_INCLUDE_DIR, PYTHON_INCLUDE_NAME)
+PREFIX_INCLUDE_DIR = Path(PREFIX, "include")
+PYTHON_INCLUDE_DIR = sysconfig.get_config_var("include")
 NVJITLINK_INCLUDE_DIR = Path(NVJITLINK_DIR, "include")
-CUDA_INCLUDE_DIR = "/usr/local/cuda/include"
 
 flags = [
     "--cuda-gpu-arch=sm_50",
-    f"-I{CONDA_INCLUDE_DIR}",
-    f"-I{CUDA_INCLUDE_DIR}",
-    f"-I{NVJITLINK_INCLUDE_DIR}",
+    f"-I{PREFIX_INCLUDE_DIR}",
     f"-I{PYTHON_INCLUDE_DIR}",
+    f"-I{NVJITLINK_INCLUDE_DIR}",
 ]
 
 
