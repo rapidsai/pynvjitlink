e1a8ecf6924a5bc5036b8f308fbffa3d893ce37c
Bradley Dice <bdice@bradleydice.com>
2024-03-07 01:30:26 -0600

    Update to CUDA 12.4 (#62)
    This PR updates pynvjitlink to use CUDA 12.4. This depends on
    https://github.com/conda-forge/libnvjitlink-feedstock/pull/9.

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -14,15 +14,11 @@ rapids-logger "Download Wheel"
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="pynvjitlink_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist/
 
-# This is the version of the suffix with a preceding hyphen. It's used
-# everywhere except in the final wheel name.
-PACKAGE_CUDA_SUFFIX="-${RAPIDS_PY_CUDA_SUFFIX}"
-
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}/
 mkdir -p "${RAPIDS_TESTS_DIR}"
 
 rapids-logger "Install wheel"
-python -m pip install --find-links ./dist pynvjitlink${PACKAGE_CUDA_SUFFIX}
+python -m pip install $(echo ./dist/pynvjitlink_${RAPIDS_PY_CUDA_SUFFIX}*.whl)
 
 rapids-logger "Build Tests"
 pushd test_binary_generation
