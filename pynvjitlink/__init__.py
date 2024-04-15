a0405def522bdaf0169d42eafdfc4219a1f98b38
Bradley Dice <bdice@bradleydice.com>
2024-04-15 17:50:32 -0500

    Fix `__version__` (#69)
    This PR fixes the `pynvjitlink.__version__` attribute to read from the
    `VERSION` file.
    
    Fixes https://github.com/rapidsai/pynvjitlink/issues/70

--- pynvjitlink/__init__.py
+++ pynvjitlink/__init__.py
@@ -1,6 +1,6 @@
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2023-2024, NVIDIA CORPORATION.
 
 from pynvjitlink.api import NvJitLinker, NvJitLinkError
+from pynvjitlink._version import __version__
 
-__all__ = (NvJitLinker, NvJitLinkError)
-__version__ = "0.0.0"
+__all__ = [NvJitLinker, NvJitLinkError, __version__]
