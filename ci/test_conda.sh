0301171396feaa1d3aca47a30e9a2216d8e7d6a7
Graham Markall <gmarkall@nvidia.com>
2024-04-19 16:11:59 +0100

    Add cuda-python testing dependency; fix copyright years

--- ci/test_conda.sh
+++ ci/test_conda.sh
@@ -12,6 +12,7 @@ rapids-mamba-retry create -n test \
     cxx-compiler \
     cuda-nvcc \
     cuda-nvrtc \
+    cuda-python \
     cuda-version=${RAPIDS_CUDA_VERSION%.*} \
     "numba>=0.58" \
     make \
