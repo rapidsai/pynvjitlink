e9d34abca14e27200698298c12a9e05b0000dfc8
James Lamb <jaylamb20@gmail.com>
2024-06-10 11:35:10 -0500

    use rapids-build-backend (#88)
    Contributes to https://github.com/rapidsai/build-planning/issues/31
    Contributes to
    https://github.com/rapidsai/dependency-file-generator/issues/89
    
    Proposes introducing `rapids-build-backend` as this project's build
    backend, to reduce the complexity of various CI/build scripts.
    
    ---------
    
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

---
+++ pynvjitlink/tests/test_version.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.
+
+import pynvjitlink
+
+
+def test_version_constants_are_populated():
+    # __git_commit__ will only be non-empty in a built distribution
+    assert isinstance(pynvjitlink.__git_commit__, str)
+
+    # __version__ should always be non-empty
+    assert isinstance(pynvjitlink.__version__, str)
+    assert len(pynvjitlink.__version__) > 0
