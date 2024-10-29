d6429dfe3ceae5f4adcb35f918155d969f50fcb2
James Lamb <jlamb@nvidia.com>
2024-10-29 10:11:36 -0500

    print sccache stats in builds

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -8,9 +8,13 @@ source "$(dirname "$0")/install_latest_cuda_toolkit.sh"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
+sccache --zero-stats
+
 rapids-logger "Build wheel"
 mkdir -p ./dist
-python -m pip wheel . --wheel-dir=./dist -vvv --disable-pip-version-check --no-deps
+python -m pip wheel . --wheel-dir=./dist -v --disable-pip-version-check --no-deps
+
+sccache --show-adv-stats
 
 # Exclude libcuda.so.1 because we only install a driver stub
 python -m auditwheel repair --exclude libcuda.so.1 -w ./final_dist ./dist/*
