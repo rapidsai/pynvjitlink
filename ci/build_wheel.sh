c89e6c28c6d8844a53a55305cd5ca149ac63d6cd
James Lamb <jlamb@nvidia.com>
2025-05-28 09:55:20 -0500

    stop uploading packages to downloads.rapids.ai, update to 25.08 workflows (#144)
    Contributes to https://github.com/rapidsai/build-planning/issues/181
    
    * removes all uploads of conda packages and wheels to `downloads.rapids.ai`
    * updates all `shared-workflows` references to `branch-25.08`, to pull in changes from https://github.com/rapidsai/shared-workflows/pull/364
    
    ## Notes for Reviewers
    
    ### Is it safe to update to `branch-25.08` of `shared-workflows` on `main` here?
    
    I'm assuming yes, because CI here computes its own custom-to-`pynvjitlink` support matrices:
    
    https://github.com/rapidsai/pynvjitlink/blob/8ea3d166fa2c0cc9372829a84c9e5d5721988895/.github/workflows/build.yaml#L16-L24
    
    All other non-matrix changes on `shared-workflows` between `branch-25.06` and `branch-25.08` should be fine for this repo.
    
    ### How I identified changes
    
    Looked for uses of the relevant `gha-tools` tools, as well as documentation about `downloads.rapids.ai`, being on the NVIDIA VPN, using S3, etc. like this:
    
    ```shell
    git grep -i -E 's3|upload|downloads\.rapids|vpn'
    ```
    
    ### How I tested this
    
    See "How I tested this" on https://github.com/rapidsai/shared-workflows/pull/364
    
    #
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/pynvjitlink/pull/144

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -10,8 +10,6 @@ wheel_dir=${RAPIDS_WHEEL_BLD_OUTPUT_DIR}
 rapids-logger "Install CUDA Toolkit"
 source "$(dirname "$0")/install_latest_cuda_toolkit.sh"
 
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
-
 sccache --zero-stats
 
 rapids-logger "Build wheel"
@@ -22,6 +20,3 @@ sccache --show-adv-stats
 
 # Exclude libcuda.so.1 because we only install a driver stub
 python -m auditwheel repair --exclude libcuda.so.1 -w "${wheel_dir}" ./dist/*
-
-rapids-logger "Upload Wheel"
-RAPIDS_PY_WHEEL_NAME="pynvjitlink_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 python "${wheel_dir}"
